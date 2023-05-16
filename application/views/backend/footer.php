a77954a1e3809f62b625e56de60639741bfc4f0d
Lionel-Johnson <104918212+Lionel-Johnson@users.noreply.github.com>
2023-05-16 20:01:58 +0100

    mise en production du site de la nasa

---
+++ application/views/backend/footer.php
@@ -0,0 +1,5 @@
+<!-- Footer -->
+<footer class="main">
+	&copy; <?php echo date('Y')?> | La Nasa G8 -  Système de Gestion des Données Personnalisées des Militants du PDG |
+    <strong>Développé par Lionel-Brice NGOUVI MOUKENDOU - LMS GABON</strong>
+</footer>
